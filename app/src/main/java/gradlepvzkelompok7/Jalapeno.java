3dbb7dbc72bc6b40fae17cd37e9fbf3c9a43edc6
thalitazhrr <140157102+thalitazhrr@users.noreply.github.com>
2024-05-18 01:55:22 +0700

    add 2 new plants: jalapeno tanglekelp

---
+++ app/src/main/java/gradlepvzkelompok7/Jalapeno.java
@@ -0,0 +1,8 @@
+package gradlepvzkelompok7;
+
+public class Jalapeno extends Plant {
+
+    public Jalapeno() {
+        super("Jalapeno", 50, 0, 0, false, 125, 0, 50);
+    }
+}
---
+++ app/src/main/java/gradlepvzkelompok7/TangleKelp.java
@@ -0,0 +1,9 @@
+package gradlepvzkelompok7;
+
+public class TangleKelp extends Plant {
+
+    public TangleKelp() {
+        super("Tangle Kelp", 50, 0, 0, true, 25, 0, 0);
+    }
+
+}
