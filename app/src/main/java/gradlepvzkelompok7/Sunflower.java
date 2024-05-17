7aede76944855d41a56d9639cb9a873d3f70aaac
thalitazhrr <140157102+thalitazhrr@users.noreply.github.com>
2024-05-17 23:38:20 +0700

    add sunflower and wallnut

---
+++ app/src/main/java/gradlepvzkelompok7/Sunflower.java
@@ -0,0 +1,7 @@
+package gradlepvzkelompok7;
+
+public class Sunflower extends Plant {
+    public Sunflower() {
+        super("Sunflower", 100, 0, 0, false, 50, 0, 10);
+    }
+}
---
+++ app/src/main/java/gradlepvzkelompok7/WallNut.java
@@ -0,0 +1,8 @@
+package gradlepvzkelompok7;
+
+public class WallNut extends Plant {
+    public WallNut() {
+        super("Wall nut", 1000, 0, 0, false, 50, 0, 20);
+    }
+
+}
